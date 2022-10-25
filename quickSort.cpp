428e1c86c486e377e9d114ee93ee419a694c6425
Robert <rcabrer005@citymail.cuny.edu>
2022-10-25 22:32:17 +0000

    added quicksort and updated past sorting algorithms

--- quickSort.cpp
+++ quickSort.cpp
@@ -7,23 +7,23 @@ int partition(vector<int> &V, int left, int right)
 {
 
 int pivot = V[right];
-int lower_segment_end = right - 1;
+int lower_segment = left - 1;
 
-for (int j = right ; j < right ; j++)
+for (int j = left ; j < right ; j++)
 {
     //if is a smaller number we extend the lower segment
     if(V[j] <= pivot){
-        lower_segment_end++;
+        lower_segment++;
 
     //swap
     int tmp = V[j];
-    V[j] = V[lower_segment_end];
-    V[lower_segment_end] = tmp;
+    V[j] = V[lower_segment];
+    V[lower_segment] = tmp;
 
     }
 }
 
-int pivot_index= lower_segment_end + 1;
+int pivot_index= lower_segment + 1;
 
 //swap
 int tmp = V[right];
@@ -40,21 +40,26 @@ void QuickSort(vector <int> &V, int left, int right)
     if(left < right)
     {
         int pivot = partition(V, left, right);
-        QuickSort(V, left, right - 1);
+
+        QuickSort(V, left, pivot - 1);
         QuickSort(V, pivot + 1, right);
     }
 }
 
 int main(){
 
-vector<int> unsorted = {5,4,3,2,1,6};
+vector<int> vec = {5,4,3,2,1,6};
 
-QuickSort(unsorted, 0, unsorted.size()-1);
-
-for(int i : unsorted)
+for(int i : vec)
     cout << i << "  ";
 
 
+cout << "\n\n";
+
+QuickSort(vec, 0, vec.size()-1);
+
+for(int i : vec)
+    cout << i << "  ";
 
     return 0;
 }
